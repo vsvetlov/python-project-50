3085fbd148f3db7b7c7e12d8526ce4b83da2a699
Vitaly S <vsvetlov@gmail.com>
2023-08-22 00:25:54 +0300

    Add gediff script

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    args = parser.parse_args()
+    print(args.first_file, args.second_file)
+
+
+if __name__ == '__main__':
+    main()
