267de8bd4ce49f5de64e12571acb66b88bfea578
Vitaly S <vsvetlov@gmail.com>
2023-08-23 13:56:37 +0300

    Update arguments list

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     print(args.first_file, args.second_file)
 
