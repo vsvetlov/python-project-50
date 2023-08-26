641033e3f620b48ab74ffead9a7c586d5582c98f
Vitaly S <vsvetlov@gmail.com>
2023-08-26 17:42:07 +0300

    Add stylish formatter

--- gendiff/gendifflib.py
+++ gendiff/gendifflib.py
@@ -23,7 +23,7 @@ def get_diff(data1, data2):
     for k in keys:
         print(k)
         if k in data1 and k in data2:
-            if type(data1[k]) == dict and type(data2[k]) == dict:
+            if type(data1[k]) is dict and type(data2[k]) is dict:
                 diff.append([' ', k, get_diff(data1[k], data2[k])])
             elif data1[k] == data2[k]:
                 diff.append([' ', k, data1[k]])
@@ -31,22 +31,39 @@ def get_diff(data1, data2):
                 diff.append(['-', k, data1[k]])
                 diff.append(['+', k, data2[k]])
         elif k in data1:
+            # if type(data1[k]) is dict:
+            #     diff.append(['-', k, get_diff(data1[k], data1[k])])
+            # else:
             diff.append(['-', k, data1[k]])
         else:
+            # if type(data2[k]) is dict:
+            #     diff.append(['+', k, get_diff(data2[k], data2[k])])
+            # else:
             diff.append(['+', k, data2[k]])
     return diff
 
 
-def format_output(diff):
+def dict_to_list(data):
+    result = []
+    for k, v in data.items():
+        result.append([' ', k, v])
+    return result
+
+
+def format_stylish(diff, lvl=0):
+    prefix = f'{" " * 4 * lvl}'
     output = ['{']
     for i in diff:
         print(i)
+        if type(i[2]) is dict:
+            i[2] = dict_to_list(i[2])
         if type(i[2]) is list:
-            output.append(f'  {i[0]} {i[1]}: {format_output(i[2])}')
+            parent = format_stylish(i[2], lvl + 1)
+            output.append(f'{prefix} {i[0]} {i[1]}: {parent}')
         else:
-            output.append(f'  {i[0]} {i[1]}: {i[2]}')
+            output.append(f'{prefix} {i[0]} {i[1]}: {i[2]}')
     # output = ['{'] + [f'  {i[0]} {i[1]}: {i[2]}' for i in diff] + ['}']
-    output.append('}')
+    output.append(f'{prefix}}}')
     return '\n'.join(output)
 
 
@@ -54,6 +71,5 @@ def generate_diff(file_path1, file_path2):
     data1, data2 = parse_files(file_path1, file_path2)
     diff = get_diff(data1, data2)
     print(diff)
-    output = format_output(diff)
-    print(f'#####\n{output}\n###########\n###########')
+    output = format_stylish(diff)
     return output
