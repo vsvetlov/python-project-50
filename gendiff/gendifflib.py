f04b37b1ab22674d3397ed22463e8702e7007dea
Vitaly S <vsvetlov@gmail.com>
2023-08-26 10:36:24 +0300

    Add yaml format

--- gendiff/gendifflib.py
+++ gendiff/gendifflib.py
@@ -1,18 +1,23 @@
 import json
+import yaml
 
 
-def parsing_files(file_path1, file_path2):
+def parse_files(file_path1, file_path2):
     with (
         open(file_path1) as f1,
         open(file_path2) as f2
     ):
-        data1 = json.load(f1)
-        data2 = json.load(f2)
+        if file_path1.endswith('json'):
+            data1 = json.load(f1)
+            data2 = json.load(f2)
+        else:
+            data1 = yaml.safe_load(f1)
+            data2 = yaml.safe_load(f2)
     return data1, data2
 
 
 def generate_diff(file_path1, file_path2):
-    data1, data2 = parsing_files(file_path1, file_path2)
+    data1, data2 = parse_files(file_path1, file_path2)
     keys = sorted(set(data1) | set(data2))
     diff = ['{']
     for k in keys:
