7a816f3a60b3d50e7ded6c7df679aca6a80be851
Vitaly S <vsvetlov@gmail.com>
2023-09-13 17:27:55 +0300

    Refactor file parsing

--- gendiff/files_parsing.py
+++ gendiff/files_parsing.py
@@ -1,13 +1,34 @@
 import json
 import yaml
 
+SUPPORTED_FORMATS = {
+    "json": "json",
+    "yaml": "yaml",
+    "yml": "yaml"
+}
+
+
+def parse(content, format_name):
+    if format_name == 'json':
+        data = json.loads(content)
+    elif format_name == 'yaml':
+        data = yaml.safe_load(content)
+    return data
+
+
+def read_file(file):
+    with open(file) as f:
+        content = f.read()
+    return content
+
 
 def parse_files(files):
-    content = []
+    data = []
     for file in files:
-        with open(file) as f:
-            if file.endswith('json'):
-                content.append(json.load(f))
-            else:
-                content.append(yaml.safe_load(f))
-    return content
+        file_format = file.split('.')[-1]
+        if file_format in SUPPORTED_FORMATS:
+            content = read_file(file)
+            data.append(parse(content, SUPPORTED_FORMATS[file_format]))
+        else:
+            raise Exception('Unsupported file format')
+    return data
